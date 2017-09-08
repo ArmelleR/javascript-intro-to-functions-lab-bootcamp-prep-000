fa4c5733aa283393fa57a711e8cdd5509cdb9cd6
Armelle Rose-Olesco <armelleroseolesco@gmail.com>
2017-09-08 18:31:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function whisper(string) {
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string {
    return "YES INDEED"}
-  else if string.toLowerCase() === string {
+  else if (string.toLowerCase()) === string {
   return  "I can't hear you")
 }
 }
